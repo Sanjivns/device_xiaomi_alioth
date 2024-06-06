55148441e57cf3eca1095538c0b9d94f8847d7de
Sanjivns <alshahab109@gmail.com>
2024-06-06 14:01:35 +0600

    alioth: Add trees in vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
+git clone https://github.com/Sanjivns/device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common
+git clone https://github.com/PocoF3Releases/vendor_xiaomi_alioth.git vendor/xiaomi/alioth
+git clone https://github.com/PocoF3Releases/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common
+git clone https://github.com/EmanuelCN/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250 --depth=1
+rm -rf hardware/xiaomi
+rm -rf packages/resources/devicesettings
+rm -rf system/libhwbinder
+git clone https://github.com/Evolution-X/hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/Sanjivns/packages_resources_devicesettings.git packages/resources/devicesettings
+git clone https://github.com/Evolution-X/system_libhwbinder.git system/libhwbinder
