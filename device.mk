cfed5c907d85bc593f1753764f29de73b537d715
=?UTF-8?q?=C2=A8raystef66=C2=A8?= <s.vanbarel@gmail.com>
2025-05-20 07:44:39 +0000

    alioth: : move TARGET_INIT_VENDOR_LIB to soong_config * vendor/lineage/config/BoardConfigSoong.mk:90: warning: TARGET_INIT_VENDOR_LIB is deprecated, please migrate to soong_config_set,libinit,vendor_init_lib
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -59,6 +59,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pocketmode.xml
 
+# Libinit
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_alioth)
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
